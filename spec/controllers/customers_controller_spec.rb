23a7fcfaaf883892969e1bd271316c6a73acc984
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2018-09-07 16:39:28 -0300

    create route

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 describe CustomersController, type: :controller do
   context 'when saving a user' do
     it 'saves in the database' do
-      post :create, {name: 'maria de silva'}
+      post :create, body: JSON.dump(name: 'maria da silva')
+
+      expect(response).to have_http_status(201)
+      expect(Customer.last.name).to eq 'maria da silva'
     end
   end
 end
