d68b7057942579e1bf67d1b917bb350137ef474c
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2018-09-19 09:11:31 -0300

    makes the app communicate with another one

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,17 @@
+require 'net/http'
+
 class CustomersController < ApplicationController
   def create
-    Customer.create(JSON.parse(request.body.read))
+    body = request.body.read
+
+    url = URI.parse('http://app-back-2:4000/customers/create')
+    request = Net::HTTP::Post.new(url.to_s)
+    request.body = body
+    Net::HTTP.start(url.host, url.port) do |http|
+      http.request(request)
+    end
+
+    Customer.create(JSON.parse(body))
 
     render status: 201
   end
