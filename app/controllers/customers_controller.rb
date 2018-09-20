d40d29881d268f8b20050717c8c60e5e8cc81301
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2018-09-20 09:13:40 -0300

    using typhoeus in request

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,15 +1,8 @@
-require 'net/http'
-
 class CustomersController < ApplicationController
   def create
     body = request.body.read
 
-    url = URI.parse('http://app-back-2:4000/customers/create')
-    request = Net::HTTP::Post.new(url.to_s)
-    request.body = body
-    Net::HTTP.start(url.host, url.port) do |http|
-      http.request(request)
-    end
+    Typhoeus.post('http://app-back-2:4000/customers/create', body: body)
 
     Customer.create(JSON.parse(body))
 
