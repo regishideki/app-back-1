0cf4385f12e04d51b1caaff08d735e75d115195c
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2018-09-21 08:32:20 -0300

    configure and use vcr cassette in customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,8 @@ class CustomersController < ApplicationController
   def create
     body = request.body.read
 
-    Typhoeus.post('http://app-back-2:4000/customers/create', body: body)
+    response = Typhoeus.post('http://app-back-2:4000/customers/create', body: body)
+    render status: 503 and return unless response.success?
 
     Customer.create(JSON.parse(body))
 
